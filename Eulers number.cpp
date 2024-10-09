15d0d25a928405d6945d4135003cbab7cd4841de
Updaty <howdaytoday@gmail.com>
2024-10-09 22:56:48 +0200

    Added some comments

--- Eulers number.cpp
+++ Eulers number.cpp
@@ -9,8 +9,11 @@ int main()
 	int N;
 	cin>>N;
 	double euler;
+
+	//our number of counted digits
 	int sum = 0;
 
+	//the number to add to euler's. Represents the last "1/k!" element
 	double addend = 1;
 
 	for(int i = 0;to_string(addend)[N+2]=='0';i++){
